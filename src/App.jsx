ea3ca9992b9e45c200477d2c7a11221161720a48
root <root@DESKTOP-RAKF29J>
2023-07-22 16:21:55 -0500

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Component } from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const location_key = import.meta.env.VITE_APP_location_key;
 
@@ -11,18 +12,20 @@ class App extends Component {
     this.state = {
       birminghamDisplayName: '',
       birminghamLon: '',
-      birminghamLat: ''
+      birminghamLat: '',
+      birminghamMap: "https://maps.locationiq.com/v3/staticmap?key=${location_key}a&center=33.5206824,-86.8024326&zoom=15"
     }
   }
 
   handleGetBirmingham = async () => {
-    let result = await axios.get("https://us1.locationiq.com/v1/search?key=pk.4431ba91d3c057a8d70476e8d0bd430a&q=birmingham&format=json");
+    let result = await axios.get(`https://us1.locationiq.com/v1/search?key=${location_key}&q=birmingham&format=json`);
     let data = result.data;
     console.log(result.data[1].display_name);
     this.setState({
       birminghamDisplayName: data[1].display_name,
-      birminghamLat: '',
-      birminghamLon: ''
+      birminghamLat: data[1].lat,
+      birminghamLon: data[1].lon,
+
     })
     console.log("complete");
   }
@@ -33,6 +36,10 @@ class App extends Component {
     return (
       <>
         <h2> {this.state.birminghamDisplayName} </h2>
+        <img src="https://maps.locationiq.com/v3/staticmap?key=pk.4431ba91d3c057a8d70476e8d0bd430a&center=33.5206824,-86.8024326&zoom=15" />
+        <h2>  {this.state.birminghamLat} </h2>
+        <h2>  {this.state.birminghamLon} </h2>
+
         <button onClick={this.handleGetBirmingham}>Explore</button>
 
       </>);
