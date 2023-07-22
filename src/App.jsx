573cacdce01d20a5ef9fae36f02130fc95ddd801
root <root@DESKTOP-RAKF29J>
2023-07-22 14:19:44 -0500

    found city

--- src/App.jsx
+++ src/App.jsx
@@ -16,27 +16,27 @@ class App extends Component {
   }
 
   handleGetBirmingham = async () => {
-    let result = await axios.get("https://us1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json");
+    let result = await axios.get("https://us1.locationiq.com/v1/search?key=pk.4431ba91d3c057a8d70476e8d0bd430a&q=birmingham&format=json");
     let data = result.data;
-    console.log(result.data[0].display_name);
+    console.log(result.data[1].display_name);
     this.setState({
-      birminghamDisplayName: data[0].display_name,
+      birminghamDisplayName: data[1].display_name,
       birminghamLat: '',
       birminghamLon: ''
     })
     console.log("complete");
   }
 
+
+
+  render() {
+    return (
+      <>
+        <h2> {this.state.birminghamDisplayName} </h2>
+        <button onClick={this.handleGetBirmingham}>Explore</button>
+
+      </>);
+  };
 }
-
-render(); {
-  return (
-    <>
-      <h2> {this.state.birminghamDisplayName} </h2>
-      <button onClick={this.handleGetBirmingham}>Explore</button>
-
-    </>);
-}
-
 
 export default App
