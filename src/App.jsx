3519f0009a3578850c4e3a01a51c6eaf2393d4cd
root <root@DESKTOP-RAKF29J>
2023-08-02 22:14:21 -0500

    changed code

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,72 @@
-import React from 'react'
-import { Component } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import { Image, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css";
 
-
 const location_key = import.meta.env.VITE_APP_location_key;
 
-
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      birminghamDisplayName: '',
-      birminghamLon: '',
-      birminghamLat: '',
-      birminghamMap: "https://maps.locationiq.com/v3/staticmap?key=${location_key}a&center=33.5206824,-86.8024326&zoom=15"
-    }
+      locationName: '',
+      locationDisplayName: '',
+      locationLon: '',
+      locationLat: '',
+      locationMap: `https://maps.locationiq.com/v3/staticmap?key=${location_key}&center=${this.state.locationLon},${this.state.locationLat}&zoom=15`
+    };
   }
 
-  handleGetBirmingham = async () => {
-    let result = await axios.get(`https://us1.locationiq.com/v1/search?key=${location_key}&q=birmingham&format=json`);
-    let data = result.data;
-    console.log(result.data[1].display_name);
+  handleGetlocation = async () => {
+    let result = await axios.get(`https://us1.locationiq.com/v1/search?key=${location_key}&q=${this.state.locationName}&format=json`);
+    let data = result.data[0]; 
     this.setState({
-      birminghamDisplayName: data[1].display_name,
-      birminghamLat: data[1].lat,
-      birminghamLon: data[1].lon,
-
-    })
+      locationDisplayName: data.display_name,
+      locationLat: data.lat,
+      locationLon: data.lon,
+      locationMap: `https://maps.locationiq.com/v3/staticmap?key=${location_key}&center=${data.lon},${data.lat}&zoom=15`
+    });
     console.log("complete");
   }
 
+  handleChange = (event) => {
+    this.setState({
+      locationName: event.target.value
+    });
+  }
 
+  handleExplore = async (event) => {
+    event.preventDefault();
+    let results = await axios.get(`https://us1.locationiq.com/v1/search?key=${location_key}&q=${this.state.locationName}&format=json`);
+    let data = results.data[0]; 
+    this.setState({
+      locationDisplayName: data.display_name,
+      locationLat: data.lat,
+      locationLon: data.lon,
+      locationMap: `https://maps.locationiq.com/v3/staticmap?key=${location_key}&center=${data.lon},${data.lat}&zoom=15`
+    });
+  }
 
   render() {
     return (
       <>
+        <form onSubmit={this.handleExplore}>
+          <label>
+            Location Name:
+            <input type="text" value={this.state.locationName} onChange={this.handleChange} />
+          </label>
+          <button type="submit">Explore</button>
+        </form>
         <div className="main">
-          <h2> {this.state.birminghamDisplayName} </h2>
-          <Image src="https://maps.locationiq.com/v3/staticmap?key=pk.4431ba91d3c057a8d70476e8d0bd430a&center=33.5206824,-86.8024326&zoom=15" />
-          <h2>  {this.state.birminghamLat} </h2>
-          <h2>  {this.state.birminghamLon} </h2>
+          <h2>{this.state.locationDisplayName}</h2>
+          <h2>{this.state.locationLat}</h2>
+          <h2>{this.state.locationLon}</h2>
+          {this.state.locationMap && <Image src={this.state.locationMap} alt="Location Map" />}
         </div>
-        <Button onClick={this.handleGetBirmingham}>Explore</Button>
-
-      </>);
+      </>
+    );
   };
 }
 
-export default App
+export default App;
