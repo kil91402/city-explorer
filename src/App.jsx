b8bedf7735aa8ae3e7b4ee6cba24229037e14ce0
root <root@DESKTOP-RAKF29J>
2023-08-02 22:43:41 -0500

    added search feature

--- src/App.jsx
+++ src/App.jsx
@@ -14,39 +14,37 @@ class App extends Component {
       locationDisplayName: '',
       locationLon: '',
       locationLat: '',
-      locationMap: `https://maps.locationiq.com/v3/staticmap?key=${location_key}&center=${this.state.locationLon},${this.state.locationLat}&zoom=15`
+      locationMap: '',
+      error: null,
     };
-  }
-
-  handleGetlocation = async () => {
-    let result = await axios.get(`https://us1.locationiq.com/v1/search?key=${location_key}&q=${this.state.locationName}&format=json`);
-    let data = result.data[0]; 
-    this.setState({
-      locationDisplayName: data.display_name,
-      locationLat: data.lat,
-      locationLon: data.lon,
-      locationMap: `https://maps.locationiq.com/v3/staticmap?key=${location_key}&center=${data.lon},${data.lat}&zoom=15`
-    });
-    console.log("complete");
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      locationName: event.target.value
-    });
   }
 
   handleExplore = async (event) => {
     event.preventDefault();
-    let results = await axios.get(`https://us1.locationiq.com/v1/search?key=${location_key}&q=${this.state.locationName}&format=json`);
-    let data = results.data[0]; 
+    try {
+      const result = await axios.get(
+        `https://us1.locationiq.com/v1/search.php?key=${location_key}&q=${this.state.locationName}&format=json`
+      );
+      const data = result.data[0];
+      this.setState({
+        locationDisplayName: data.display_name,
+        locationLat: data.lat,
+        locationLon: data.lon,
+        locationMap: `https://maps.locationiq.com/v3/staticmap?key=${location_key}&center=${data.lat},${data.lon}&zoom=15`,
+        error: null,
+      });
+    } catch (error) {
+      this.setState({
+        error: error.response.status + ' ' + error.response.data.error,
+      });
+    }
+  };
+
+  handleChange = (event) => {
     this.setState({
-      locationDisplayName: data.display_name,
-      locationLat: data.lat,
-      locationLon: data.lon,
-      locationMap: `https://maps.locationiq.com/v3/staticmap?key=${location_key}&center=${data.lon},${data.lat}&zoom=15`
+      locationName: event.target.value,
     });
-  }
+  };
 
   render() {
     return (
@@ -56,17 +54,21 @@ class App extends Component {
             Location Name:
             <input type="text" value={this.state.locationName} onChange={this.handleChange} />
           </label>
-          <button type="submit">Explore</button>
+          <button type="submit">Explore!</button>
         </form>
-        <div className="main">
-          <h2>{this.state.locationDisplayName}</h2>
-          <h2>{this.state.locationLat}</h2>
-          <h2>{this.state.locationLon}</h2>
-          {this.state.locationMap && <Image src={this.state.locationMap} alt="Location Map" />}
-        </div>
+        {this.state.error ? (
+          <div className="error">{this.state.error}</div>
+        ) : (
+          <div className="main">
+            <h2>{this.state.locationDisplayName}</h2>
+            <h2>{this.state.locationLat}</h2>
+            <h2>{this.state.locationLon}</h2>
+            {this.state.locationMap && <Image src={this.state.locationMap} alt="Location Map" />}
+          </div>
+        )}
       </>
     );
-  };
+  }
 }
 
 export default App;
